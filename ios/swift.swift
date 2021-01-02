84978744547b368faa6849febe797aba3a12fbf9
Nikhil Gaur <ngaur6834@gmail.com>
2021-01-02 16:54:16 +0530

    sound on

---
+++ ios/swift.swift
@@ -0,0 +1,9 @@
+//
+//  swift.swift
+//  Runner
+//
+//  Created by Nikhil Gaur on 16/12/20.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int n) {
+    final player = AudioCache();
+    player.play('note$n.wav');
+  }
+
+  Expanded buildSec(int i, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(i);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildSec(1, Colors.red),
+              buildSec(2, Colors.orange),
+              buildSec(3, Colors.yellow),
+              buildSec(4, Colors.green),
+              buildSec(5, Colors.blue),
+              buildSec(6, Colors.teal),
+              buildSec(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
